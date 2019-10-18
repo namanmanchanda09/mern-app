0db8443e4f4434b88cebfcabdbf86f0530b1652f
namanmanchanda09 <naman.mohan09@outlook.com>
2019-10-19 01:19:05 +0530

    CRUD operations

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+// import ListItemText from '@material-ui/core/ListItemText';
+import * as compose from 'lodash.flowright';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const UsersQuery = gql`
+{
+  users{
+    id
+    name
+    adult
+  }
+}
+`;
+
+const UpdateMutation = gql`
+mutation($id:ID!, $adult:Boolean!){
+  updateUser(id:$id,adult:$adult)
+}
+`;
+
+const RemoveMutation = gql`
+mutation($id:ID!){
+  removeUser(id:$id)
+}
+`;
+
+
+
+class App extends React.Component{
+  updateUser = async user =>{
+    await this.props.updateUser({
+      variables :{
+        id: user.id,
+        adult: !user.adult
+      }
+    })
+  };
+
+  removeUser = async user =>{
+    await this.props.removeUser({
+      variables:{
+        id: user.id
+      }
+    })
+  }
+
+  render(){
+    const {data : {loading, users}} = this.props;
+    if(loading){
+      return null;
+    }
+    return users.map(user => (
+      <div key={`${user.id}-user-number`} className="App" onClick={() => (this.removeUser(user))}>
+        <List component="nav" aria-label="secondary mailbox folders">
+          <ListItem button>
+            Name : {user.name}
+            <br />
+            Adult : {user.adult.toString()}
+          </ListItem>
+          <ListItemSecondaryAction>
+                      <IconButton>
+                        <CloseIcon />
+                      </IconButton>
+          </ListItemSecondaryAction>
+          
+
+        </List>
+      </div>
+      )
+    )
+  }
 }
 
-export default App;
+export default compose(
+graphql(RemoveMutation, {name: 'removeUser'}),
+graphql(UpdateMutation, {name: 'updateUser'}),
+(graphql(UsersQuery)))(App);
+
+
+
+
+
+
+
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "http://localhost:4000"
+});
+
+
+ReactDOM.render(<ApolloProvider client={client}>
+                <App />
+                </ApolloProvider>,
+             document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
